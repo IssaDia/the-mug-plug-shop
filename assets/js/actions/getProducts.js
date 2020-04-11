09093fd9c5dcbbaf1fcd0549ff972757ef578131
IssaDia <issadiapro@gmail.com>
2020-04-11 18:27:10 +0200

    Deploying on Heroku

--- assets/js/actions/getProducts.js
+++ assets/js/actions/getProducts.js
@@ -1,6 +1,8 @@
 import { FETCH_PRODUCTS } from './type';
 import axios from 'axios';
 
+const API_URL = process.env.API_URL;
+
 {
   /* action to get all products from database via axios */
 }
@@ -15,7 +17,7 @@ export const getProductsFromApi = (products) => {
 export const getProducts = () => {
   return (dispatch) => {
       axios 
-      .get("http://127.0.0.1:8000/api/products",{
+      .get( API_URL + "products",{
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
