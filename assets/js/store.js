f7909978ea45d15924467ce8ef50602473d89ee6
IssaDia <issadiapro@gmail.com>
2020-04-11 21:47:15 +0200

    Deploy: test with new env set up on Heroku

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,27 +1,33 @@
-import {createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
+import {
+     createStore,
+     applyMiddleware
+} from "redux";
+import {
+     composeWithDevTools
+} from "redux-devtools-extension";
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { getProducts } from './actions/getProducts';
+import {
+     getProducts
+} from './actions/getProducts';
 
 
 {
      /*Store setup with combined reducers tools for Chrome */
-   }
+}
 
 const middleware = [thunk];
 
 const store = createStore(rootReducer,
-    
+
      composeWithDevTools(applyMiddleware(...middleware))
 );
 
 
 {
      /*Get all products from database at start*/
-   }
+}
 
 store.dispatch(getProducts());
 
 export default store;
-
